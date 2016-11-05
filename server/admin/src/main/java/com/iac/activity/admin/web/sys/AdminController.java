79aa1c4c8ddda637429bb6fdc3fbf14a2f73295b
Markin <iacdingping@gmail.com>
2016-11-05 19:51:14 +0800

    example

--- server/admin/src/main/java/com/iac/activity/admin/web/sys/AdminController.java
+++ server/admin/src/main/java/com/iac/activity/admin/web/sys/AdminController.java
@@ -1,10 +1,17 @@
 package com.iac.activity.admin.web.sys;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.iac.activity.admin.web.AbstractController;
 
 @Controller
+@RequestMapping
 public class AdminController extends AbstractController {
 
+    @RequestMapping(value="login", method=RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
 }
